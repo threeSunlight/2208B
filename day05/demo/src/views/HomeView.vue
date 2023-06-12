43b1c0f0223a9df4d5d11c391b0a05d351384884
wbj <709965309@qq.com>
2023-06-12 09:09:31 +0800

    feat: 知识点总结

---
+++ day05/demo/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+// import data from "../../mock/data.json";
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+  mounted() {
+    // console.log(data, "data");
+    const data = require("../../mock/data.json");
+    console.log(data, "data");
+  },
+};
+</script>
